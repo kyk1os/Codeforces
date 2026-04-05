cfe57125942cd21ccf2cc424f692cc366481535c
kyk1os <kyzhle0613@gmail.com>
2026-04-05 16:55:56 +0800

    default

---
+++ practice/edu184A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
