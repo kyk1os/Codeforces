08769d5922cbca16081842aa37e29e15ccc8a059
kyk1os <kyzhle0613@gmail.com>
2026-04-14 22:28:41 +0800

    default

---
+++ practice/1058A.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> arr(n);
+    for (int i=0;i<n;i++) cin >> arr[i];
+
+    vector<int> freq(105);
+    for (int x:arr) freq[x]++;
+
+    int ans=0;
+    while (freq[ans]>0) ans++;
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+    while (T--) solve();
+    return 0;
+}
