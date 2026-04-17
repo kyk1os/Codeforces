297aacc8ce179e8cbcdd51c6e6a67f2dba127273
kyk1os <kyzhle0613@gmail.com>
2026-04-17 11:01:41 +0800

    default

--- practice/edu183B.cpp
+++ practice/edu183B.cpp
@@ -15,7 +15,24 @@ using namespace std;
 using ll = long long;
 
 void solve() {
-    
+    int n,k; cin >> n >> k;
+    string s; cin >> s;
+
+    int c0=0,c1=0,c2=0;
+    for (char x:s){
+        if (x=='0') c0++;
+        else if (x=='1') c1++;
+        else c2++;
+    }
+
+    string ans;
+    for (int i=1;i<=n;i++){
+        if (i<=c0 || i>n-c1) ans.push_back('-');
+        else if (i>=c0+c2+1 && i<=n-c1-c2) ans.push_back('+');
+        else ans.push_back('?');
+    }
+
+    cout << ans << "\n";
 }
 
 int main() {
